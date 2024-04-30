5c7d2fd8a25b3edbf01cc8a0533a1be6bb2d4e0f
rcorenti <rcorenti@student.42.fr>
2024-04-30 23:19:52 +0200

    improvements

--- script.js
+++ script.js
@@ -2,24 +2,37 @@ document.getElementById('autoButton').addEventListener('click', auto);
 document.getElementById('linesNumber').addEventListener('click', refresh);
 document.getElementById('stopButton').addEventListener('click', stop);
 document.getElementById('resetButton').addEventListener('click', reset);
+document.getElementById('linesColor').addEventListener('input', changeLinesColor);
 
 let autoOn = false;
+let reseted = false;
 let memory = 0;
+let linesColor = '#ff0000';
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function changeLinesColor() {
+    linesColor = document.getElementById('linesColor').value;
 }
 
 async function auto() {
     let i = 1;
     if (autoOn)
         return;
-    i = memory;
+    if (!reseted) {
+        i = memory;
+        reseted = false;
+    }
     autoOn = true;
     while (autoOn) {
+        if (i > 1500) {
+            break;
+        }
         draw(i);
         document.getElementById('linesNumber').value = i;
-        await sleep(1);
+        await sleep(1 + ((document.getElementById('speedRange').value - 1) * 199) / 99);
         i++;
     }
     memory = i;
@@ -31,6 +44,7 @@ function stop() {
 
 function reset() {
     autoOn = false;
+    reseted = true;
     draw(0);
     memory = 0;
 }
@@ -54,6 +68,7 @@ function draw(nbOfLines) {
     for (let i = 0; i < 360; i += inc) {
         let needle = document.createElement('div');
         needle.className = 'needle';
+        needle.style.backgroundColor = linesColor;
         needle.style.transform = `rotate(${i}deg)`;
         circle.appendChild(needle);
     }
