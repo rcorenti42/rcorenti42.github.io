2795719556fec2757bd69010ff7f28853a3b97c3
rcorenti <rcorenti@student.42.fr>
2024-04-30 12:38:04 +0200

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+// Récupération du contexte du canevas
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+// Définition des propriétés du cercle
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+var radius = 50;
+var color = "red";
+
+// Dessin du cercle
+ctx.beginPath();
+ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+ctx.fillStyle = color;
+ctx.fill();
+ctx.closePath();
